30a3ec89457ab29e3782be6c1f61f2a939b91a72
yao-ying228 <wowowo228@msn.com>
2021-06-21 21:15:03 +0800

    add check file function

--- product-2.py
+++ product-2.py
@@ -1,12 +1,18 @@
+import os #operating systeam
+
 #讀取檔案
 products = []
-with open('products.csv' , 'r' , encoding = 'utf-8') as f :
-    for line in f :
-        if '商品 , 價格 ' in line :
-            continue        
-        name , price =line.strip().split(',')
-        products.append([name , price])
-print(products)        
+if os.path.isfile('products.csv'): #檢查檔案在不在
+    print('太棒了,找到檔案了')
+    with open('products.csv' , 'r' , encoding = 'utf-8') as f :
+        for line in f :
+            if '商品 , 價格 ' in line :
+                continue        
+            name , price =line.strip().split(',')
+            products.append([name , price])
+    print(products)      
+else:
+    print('糟糕!檔案遺失了...')
 
 #讓使用者輸入
 while True:
@@ -21,7 +27,7 @@ print(products)
 #印出所有購買紀錄
 for p in products :
 	print(p[0] , '的價錢是' , p[1])
-    
+
 #將資料寫入
 with open('products.csv' , 'w' , encoding ='utf-8') as f : 
     f.write('商品 , 價格 \n')
